eb76733b7ea17e277d9ab6ded26dec0ca8b57252
Kitajima2910 <kitajima2910@gmail.com>
2019-07-30 22:53:40 +0700

    First Push

---
+++ HelloWorld/SayHello.py
@@ -0,0 +1,5 @@
+print("Làm chủ python trong 4 tuần: ")
+x = 5
+y = 10
+z = x + y
+print("{0} + {1} = {2}".format(x, y, z))
---
+++ HocKieuDuLieu/KieuDuLieu.py
@@ -0,0 +1,11 @@
+x = 5
+print(type(x))
+x = 5.5
+print(type(x))
+x = 'Hello Python!'
+print(type(x))
+x = True
+print(type(x))
+x = complex(5, 6)
+print(type(x))
+print('Thực: {0} - Ảo: {1}'.format(x.real, x.imag))
