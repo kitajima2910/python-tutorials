2795d52d4099fb12bab90a34431ef060eb15d63c
Kitajima2910 <kitajima2910@gmail.com>
2019-08-02 15:39:35 +0700

    Dang dung lai bai 40

---
+++ CacHamToanHoc/HamToanHoc.py
@@ -0,0 +1,9 @@
+from math import *
+
+print(pow(2, 3))
+print(2 ** 3)
+print(sqrt(25))
+print('30 deg = {0} radians'.format(radians(30)))
+print('0.5235987755982988 radians = {0} deg'.format(degrees(0.5235987755982988)))
+print(log10(100))
+# sin, cos, tan, 1/tan
---
+++ HamRound/HocRound.py
@@ -0,0 +1,3 @@
+print(round(5 / 7, 3))
+print(round(5 / 7))
+print(round(5 / 7, 0))
